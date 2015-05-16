dd7ad3adff442db8a3e42bdf2b44048ef5a65553
hamidre13 <nick@seodapop.com>
2015-05-15 22:18:55 -0700

    code clean  up,ability to support value sets,up to 10 charts per page, temperory solve #1

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ZingChart-wordpress
-This plugin will add Zing charts javascript library to wordpress
+This plugin will add Zing charts javascript library to wordpress.
+For installation:
+Download the code az a zip file and upload it to your wordpress site.
+In order to use it:
+Simply put this shortcode into your page:
+[zing type="" values1="" values2="" ..... height="[optional]" weidth="[optional]"]
--- zing_chart.php
+++ zing_chart.php
@@ -5,47 +5,74 @@ Plugin URI:http://www.zingchart.com/
 Description: This plugin adds Zing charts to wordpress.
 Author: Hamid Tavakoli
 Version:1.0
-Author URI: http://seodapop.com
+Author URI: http://pint.com
 
 
 Lisense :???
 */
 function zing_activate(){
-	//Probably add js liberarry
-  error_log("Zing_chart activated");
+  //Just a place holder
 }
 register_activation_hook(__FILE__,"zing_activate");
 function zing_deactivate(){
-
+  //Just another place holder
 }
 register_deactivation_hook(__FILE__,"zing_deactivate");
 
 function zing_plot($atts,$content=null){
-  STATIC $diveId = 0;
-  $diveId++;
-  $atts = shortcode_atts(array('type' => 'bar','xvals' => '' , 'yvals' => '',
-    'height' =>'400','width' => '600'),$atts);
+  STATIC $divId = 0;
+  $divId++;
+  echo("in plot chartCount is:".$chartCount);
+  /* If we need to have deafult vals we have to use this otherise,user can define everything
+  $atts = shortcode_atts(array('type' => 'bar','vals1' => '' , 'vals2' => '','vals3'=>'','vals4'=>'','vals5'=>'',
+    'vals6'=>'','vals7'=>'','vals8'=>'','vals9'=>'','vals10'=>'','height' =>'400','width' => '600'),$atts);*/
+
+  $valueHolders = '';  
+  foreach($atts as $key => $value){
+    if (!empty($value)) {
+      if (strcmp($key,'type') && strcmp($key, 'height') && strcmp($key, 'width')) {
+          $valueHolders .= '{ "values": ['.$value.'] },';
+      }
+      
+    }
+  }
+  $valueHolders =  rtrim($valueHolders,",");
   return '
   <script>
-  var chartData'.$diveId.'={
+  var chartData'.$divId.'={
     "type":"'.$atts["type"] .'",
     "series": [
-      { "values": ['.$atts["xvals"].'] },
-      { "values": ['.$atts["yvals"].'] }
+      '.$valueHolders.'
     ]
   };
-  window.onload=function(){
-    zingchart.render({
-      id:"chartDiv'.$diveId.'",
-      height:'.$atts["height"].',
-      width:'.$atts["width"].',
-      data:chartData'.$diveId.'
-    });
-  };
-  </script>  
-  <div id="chartDiv'.$diveId.'"></div>';
+  </script> 
+  <div id="chartDiv'.$divId.'"></div>';  
 }
 add_shortcode("zing","zing_plot");
+
+function zing_window_onload($content){
+  $windowLoader = '';
+  $objectLoop   = '';
+  if(is_feed()) {
+    return $content;
+  }
+  
+  for($i =1; $i<= 10;$i++) {
+            $objectLoop .= 'zingchart.render({';
+            $objectLoop .= 'id:"chartDiv'.$i.'",';
+            $objectLoop .= 'height: 600,';
+            $objectLoop .= 'width: 400,';
+            $objectLoop .= 'data:chartData'.$i.'});';       
+  }  
+  $windowLoader = '
+    <script>
+      window.onload=function(){'.
+        $objectLoop
+      .'};
+    </script>';
+  return $content.$windowLoader;
+}
+add_action('the_content','zing_window_onload');
 
 function zing_admin(){
 ?>
@@ -54,7 +81,7 @@ function zing_admin(){
     <h2>Zing Chart</h2>
     <p>Welcome to Zing Chart plugin.<br>
     In order to use this plugin simply add the following shortcode to your text <br>
-    [zing type="" xvals="" yvals="" height="[optional]" weidth="[optional]"]</p>
+    [zing type="" values1="" values2="" ..... height="[optional]" weidth="[optional]"]</p>
   </div>
 <?php
 }
